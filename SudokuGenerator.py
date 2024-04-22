d447d758f7f67286a9136c8aa360f0f7ea35eb05
namtran1812 <158846154+namtran1812@users.noreply.github.com>
2024-04-22 11:19:33 -0400

    check valid ver 1

--- SudokuGenerator.py
+++ SudokuGenerator.py
@@ -3,6 +3,7 @@ class SudokuGenerator:
         self.size = row_length
         self.removed_cells = removed_cells
         self.board = [[0 for _ in range(row_length)] for _ in range(row_length)]
+
     def get_board(self): # Returns a 2D python list of numbers, which represents the board
         return self.board
     
@@ -11,15 +12,31 @@ class SudokuGenerator:
             print(" ".join(str(num) if num != 0 else '.' for num in row))
 
     def valid_in_row(self, row, num): # Returns a Boolean value. Determines if num is contained in the given row of the board.
+        return num not in self.board[row]
 
     def valid_in_col(self, col, num): # Returns a Boolean value. Determines if num is contained in the given column of the board.
-    
+        return num not in [row[col] for row in self.board]
+
     def valid_in_box(self, row_start, col_start, num): # Returns a Boolean value. Determines if num is contained in the 3x3 box from (row_start, col_start) to (row_start+2, col_start+2)
+        for i in range(3):
+            for j in range (3):
+                if self.board[row_start + i][col_start + j] == num:
+                    return False
+            return True
 
     def is_valid(self, row, col, num): # Returns if it is valid to enter num at (row, col) in the board. This is done by checking the appropriate row, column, and box.
-    
-    def fill_box(self, row_start, col_start): # Randomly fills in values in the 3x3 box from (row_start, col_start) to (row_start+2, col_start+2). Uses unused_in_box to ensure no value occurs in the box more than once.
+        return self.valid_in_row(row, num) and self.valid_in_col(col, num) and self.valid_in_box(row - row % 3, col - col % 3, num)
 
+    def fill_box(self, row_start, col_start): # Randomly fills in values in the 3x3 box from (row_start, col_start) to (row_start+2, col_start+2). Uses unused_in_box to ensure no value occurs in the box more than once.
+        núm = [n for n in range(1, 10)]
+        random.shuffle(nums)
+        for i in range(3):
+            for j in range(3):
+                while True:
+                    num = nums.pop()
+                    if self.valid_in_box(row_start, col_start, num):
+                        self.board[row_start + i][col_start + j] = num
+                        break
     def fill_diagonal(self): # Fills the three boxes along the main diagonal of the board. This is the first major step in generating a Sudoku.
         for i in range(0, self.size, 3):
             self.fill_box(i, i)
