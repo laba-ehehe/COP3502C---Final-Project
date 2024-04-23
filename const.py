5b2fede130744eb80422d651a2a0d80024461f68
laba-ehehe <donguyenlananh29052005@gmail.com>
2024-04-23 17:33:48 -0400

    hehe

--- const.py
+++ const.py
@@ -1,7 +1,13 @@
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY = (120, 120, 120)
+# Colors - BLACK & WHITE
+# WHITE = (255, 255, 255)
+# BLACK = (0, 0, 0)
+# GRAY = (120, 120, 120)
+# PINK = (254, 127, 156)
+
+# Colors - PINK VERSION
+WHITE = (255, 209, 220)
+BLACK = (169, 64, 128)
+GRAY = (231, 84, 120)
 PINK = (254, 127, 156)
 
 # Dimensions
