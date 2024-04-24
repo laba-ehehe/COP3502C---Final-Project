2683b808ee4790747b04d8b5d71f1834d14e413a
laba-ehehe <donguyenlananh29052005@gmail.com>
2024-04-24 01:50:45 -0400

    hehe

--- Cell.py
+++ Cell.py
@@ -17,28 +17,7 @@ class Cell:  # This class represents a single cell in the Sudoku board. There ar
         else:
             self.can_be_cleared = True
 
-        # self.value = value
-        # self.row = row
-        # self.col = col
-        # self.screen = screen
-        # cell_size = width // 9
-        # self.rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
-        # self.is_given = True if value != 0 else False
-        # self.font = pygame.font.Font(None, int(cell_size * 0.6))  # Font for displaying text
-        # self.sketched_value = None  # Initialize sketched_value attribute to None
-
     def draw(self, screen):
-        # pygame.draw.rect(screen, BLACK, self.rect, 1)  # Draw cell outline
-        #
-        # if self.value != 0:
-        #     text_surface = self.font.render(str(self.value), True, BLACK)  # Render cell value
-        #     screen.blit(text_surface, (self.rect.x + 20, self.rect.y + 15))  # Draw cell value
-        # else:
-        #     if self.sketched_value is not None:
-        #         # Render sketched value if exists
-        #         sketched_text_surface = self.font.render(str(self.sketched_value), True, GRAY)
-        #         screen.blit(sketched_text_surface, (self.rect.x + 5, self.rect.y + 5))
-
         font = pygame.font.Font(None, 50)
 
         # preset numbers are black
